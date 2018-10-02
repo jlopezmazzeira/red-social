23e95fbc5792af5fd340e660c09c1afddaa252d2
jlopezmazzeira <jlopezmazzeira@gmail.com>
2018-10-02 11:25:46 -0300

    Se empieza a implementar symfony

---
+++ symfony/src/BackendBundle/Entity/Like.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Likes
+ *
+ * @ORM\Table(name="likes", indexes={@ORM\Index(name="fk_likes_users", columns={"user"}), @ORM\Index(name="fk_likes_publication", columns={"publication"})})
+ * @ORM\Entity
+ */
+class Like
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \Publication
+     *
+     * @ORM\ManyToOne(targetEntity="Publications")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="publication", referencedColumnName="id")
+     * })
+     */
+    private $publication;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="Users")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set publication
+     *
+     * @param \BackendBundle\Entity\Publication $publication
+     *
+     * @return Like
+     */
+    public function setPublication(\BackendBundle\Entity\Publication $publication = null)
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+
+    /**
+     * Get publication
+     *
+     * @return \BackendBundle\Entity\Publication
+     */
+    public function getPublication()
+    {
+        return $this->publication;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \BackendBundle\Entity\User $user
+     *
+     * @return Like
+     */
+    public function setUser(\BackendBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \BackendBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
