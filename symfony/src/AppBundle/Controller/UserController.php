6e8c131f4ad95444b0cc18dbc1c64188bfd99895
jlopezmazzeira <jlopezmazzeira@gmail.com>
2018-10-04 15:34:34 -0300

    WIP

--- symfony/src/AppBundle/Controller/UserController.php
+++ symfony/src/AppBundle/Controller/UserController.php
@@ -5,7 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use BackendBundle\Entity\User;
+use AppBundle\Form\RegisterType;
 /**
  *
  */
@@ -17,6 +18,17 @@ class UserController extends Controller
     return $this->render('AppBundle:User:login.html.twig', array(
       'titulo' => 'Login' )
     );
+  }
+
+  public function registerAction(Request $request)
+  {
+    $user = new User();
+    $form = $this->createForm(RegisterType::class, $user);
+
+    return $this->render('AppBundle:User:register.html.twig', array(
+      'form' => $form->createView()
+    ));
+
   }
 }
 
